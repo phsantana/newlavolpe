b6cb96eac12bf736b64e1a9e7d03d8e411197054
phsantana <ph_santana@outlook.com>
2019-04-27 22:58:38 -0300

    First commit

---
+++ public/js/parallax.js
@@ -0,0 +1,14 @@
+window.addEventListener("load",parallax);
+window.addEventListener("scroll",parallax);
+
+function parallax(){
+
+	[].forEach.call(document.querySelectorAll(".parallax"),parallax => {
+
+		var roll = -(parallax.getBoundingClientRect().top/5);
+
+		var bg = "calc(50% + " + roll + "px)";
+
+		parallax.style.backgroundPosition = 'center ' + bg;
+	});
+}
